8d6a6218fb0016973558656af63b33daccd70378
janeriongcol <annajaneriongcol@gmail.com>
2013-03-05 01:08:41 +0800

    What we have so far

---
+++ src/javagame/Game.java
@@ -0,0 +1,45 @@
+package javagame;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+/*
+ * Master class that controls everything.
+ */
+
+public class Game extends StateBasedGame{
+	
+	public static final String gamename = "LIZARD SPOCK 3D!";
+	public static final int menu = 0; //Menu Screen
+	public static final int choose = 1; //Choose Character Screen
+	public static final int play = 2; //Gameplay Screen
+	
+	public Game(String gamename) {
+		super(gamename); //Add title to title bar
+		//Add each state/screen individually
+		this.addState(new Menu(menu));
+		this.addState(new Choose(choose));
+		this.addState(new Play(play));
+	}
+	
+	public void initStatesList(GameContainer gc) throws SlickException{
+		this.getState(menu).init(gc, this);
+		this.getState(choose).init(gc, this);
+		this.getState(play).init(gc, this);
+		//Show Menu Screen first
+		this.enterState(menu);
+	}
+	
+	public static void main(String args[]) {
+		AppGameContainer appgc;
+		try {
+			//Create the window that will hold the game
+			appgc = new AppGameContainer(new Game(gamename));
+			appgc.setDisplayMode(500, 500, false); //false for NO FULLSCREEN
+			appgc.start();
+		} catch(SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
